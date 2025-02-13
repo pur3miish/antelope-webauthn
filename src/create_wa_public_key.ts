1db2eba74256da52a8b36c055dfdfc6f0338be6d
H <pur3miish@me.com>
2025-02-13 14:43:33 +0700

    Removed JSDoc comments in favour of ts comments.

--- src/create_wa_public_key.ts
+++ src/create_wa_public_key.ts
@@ -1,40 +1,48 @@
 import antelopeWebAuthnPublicKey from "./_utils/webauthn-public-key.js";
 
-/**
- * Antelope based web authn public key, (browser only)
- * @param {Object} Arg Argument
- * @param {Uint8Array} id
- * @param {String} email User email address or username
- * @param {String} displayName Users display name
- * @param {String} relayingParty The website url to be signed from.
- * @param {Uin8Array} challenge random challenge.
- * @returns {Object} Antelope public key, credential ID and spki public key for used for verification.
- */
-
-interface createWebAuthnKeyArgs {
+export type createWebAuthnKeyArgs = {
+  /**
+   * the ID for the PublicKeyCredentialUserEntity.
+   */
   id: Uint8Array;
+  /**
+   * User email address or username
+   */
   email: string;
+  /**
+   * Users display name
+   */
   displayName: string;
   /**
    * Website URL where the signing will be done.
    */
   relayingParty: string;
+  /**
+   * Challenge to be signed.
+   */
   challenge: Uint8Array;
-}
+};
+
+export type device_key = {
+  id: Uint8Array;
+  public_key: string;
+  credential_id: string;
+};
+
 export default async function createWebAuthnKey({
-  id = new Uint8Array(16),
+  id = crypto.getRandomValues(new Uint8Array(16)),
   email,
   displayName,
   relayingParty,
   challenge,
-}: createWebAuthnKeyArgs) {
+}: createWebAuthnKeyArgs): Promise<device_key> {
   if (!navigator?.credentials?.create)
     throw new Error("Browser does not support webauthn.");
 
   const cred = (await navigator.credentials.create({
     publicKey: {
       rp: { id: relayingParty, name: relayingParty },
-      user: { id, name: email, displayName },
+      user: { id, name: email, displayName } as PublicKeyCredentialUserEntity,
       pubKeyCredParams: [
         { type: "public-key", alg: -7 },
         { type: "public-key", alg: -257 },
@@ -48,5 +56,5 @@ export default async function createWebAuthnKey({
   const response = cred.response as AuthenticatorAttestationResponse;
   const antelope_public_key = await antelopeWebAuthnPublicKey(response);
 
-  return { public_key: antelope_public_key, credential_id: cred.id };
+  return { public_key: antelope_public_key, credential_id: cred.id, id };
 }
