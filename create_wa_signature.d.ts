199991608f056cf9365b3565a214848c8b7ec7bd
H <pur3miish@me.com>
2025-02-13 14:40:48 +0700

    device key ts type is now exported.

--- create_wa_signature.d.ts
+++ create_wa_signature.d.ts
@@ -1,6 +1,6 @@
-type device_key = {
+export type device_key = {
+    id: string;
     public_key: string;
     credential_id: string;
 };
-export default function createWebAuthnSignature(device_key: device_key, hash: Uint8Array | string): Promise<string>;
-export {};
+export default function createWebAuthnSignature(device_keys: device_key[], hash: Uint8Array | string): Promise<string>;
